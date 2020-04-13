f59ee4793b2acc915ad55c775cb5ff922b6b4af1
kharioki <antolisah@gmail.com>
2020-04-13 23:00:52 +0300

    proptype inheritance

--- objects.js
+++ objects.js
@@ -15,8 +15,22 @@ User.prototype.logout = function() {
     console.log(this.email, 'has logged out');
 }
 
+function Admin(...args){
+    User.apply(this, args);
+    this.role = 'super admin';
+}
+
+Admin.prototype = Object.create(User.prototype);
+
+Admin.prototype.deleteUser = function(u){
+    users = users.filter(user => user.email !== u.email);
+}
+
 var userOne = new User('june@ninjas.com', 'June');
 var userTwo = new User('tony@ninjas.com', 'Tony');
 
-console.log(userOne);
-userTwo.login();
+var admin = new Admin('stark@ninjas.com', 'Stark');
+
+var users = [userOne, userTwo, admin];
+
+console.log(users);
