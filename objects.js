97189ee474a8e21987fd02ccbf80f2fa48462888
kharioki <antolisah@gmail.com>
2020-04-13 22:31:15 +0300

    emulating classes using the constructor function

--- objects.js
+++ objects.js
@@ -1,43 +1,15 @@
-// classes
-
-class User {
-    constructor(email, name) {
-        // object properties
-        this.email = email;
-        this.name = name;
-        this.score = 0;
+// construcors (under the hood)
+function User(email, name) {
+    this.email = email;
+    this.name = name;
+    this.online = false;
+    this.login = function(){
+        console.log(this.email, 'has logged in');
     }
-    // methods
-    login(){
-        console.log(this.email, 'just logged in');
-        return this;
-    }
-    logout(){
-        console.log(this.email, 'just logged out');
-        return this;
-    }
-    updateScore(){
-        this.score++;
-        console.log(this.email, 'Score is now, ', this.score);
-        return this;
-    }
-};
-
-class Admin extends User {
-    deleteUser(user){
-        users = users.filter(u => {
-            return u.email != user.email;
-        })
-    }
-};
+}
 
 var userOne = new User('june@ninjas.com', 'June');
 var userTwo = new User('tony@ninjas.com', 'Tony');
 
-var admin = new Admin('stark@ninjas.com', 'Stark');
-
-var users = [userOne, userTwo, admin];
-
-admin.deleteUser(userTwo);
-
-console.log(users);
+console.log(userOne);
+userTwo.login();
