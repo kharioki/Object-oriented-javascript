0e1e2f66aa17ab10bc5d6c3a6de06b8cebd10ace
kharioki <antolisah@gmail.com>
2020-04-13 17:27:40 +0300

    class methods

--- objects.js
+++ objects.js
@@ -2,13 +2,21 @@
 
 class User {
     constructor(email, name) {
+        // object properties
         this.email = email;
         this.name = name;
+    }
+    // methods
+    login(){
+        console.log(this.email, 'just logged in');
+    }
+    logout(){
+        console.log(this.email, 'just logged out');
     }
 };
 
 var userOne = new User('june@ninjas.com', 'June');
 var userTwo = new User('tony@ninjas.com', 'Tony');
 
-console.log(userOne);
-console.log(userTwo);
+userOne.login();
+userTwo.logout();
